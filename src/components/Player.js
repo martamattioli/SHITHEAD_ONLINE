4c895f9a0134dec2ca6ddaf3ef7388b56dee5866
martapz <marta.g.mattioli@gmail.com>
2018-01-21 17:35:24 +0000

    added styling to game board

--- src/components/Player.js
+++ src/components/Player.js
@@ -5,17 +5,28 @@ const Player = ({ name, hand }) => {
   return (
     <div className="player">
       <h2>{name}</h2>
-      <div className="face-down">
-        {hand.faceDown.map(card =>
-          <div className="card" key={card.id}></div>
-        )}
-      </div>
-      <div className="face-up">
-        {hand.faceUp.map(card =>
-          <div className="card" key={card.id}>
-            {`${card.suit} ${card.value}`}
-          </div>
-        )}
+      <div className="hand">
+        <div className="face-down">
+          {hand.faceDown.map(card =>
+            <div className="card" key={card.id}></div>
+          )}
+        </div>
+        <div className="face-up">
+          {hand.faceUp.map(card =>
+            <div className="card" key={card.id}>
+              <span>{card.suit}</span>
+              <span>{card.value}</span>
+            </div>
+          )}
+        </div>
+        <div className="in-hand">
+          {hand.inHand.map(card =>
+            <div className="card" key={card.id}>
+              <span>{card.suit}</span>
+              <span>{card.value}</span>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
